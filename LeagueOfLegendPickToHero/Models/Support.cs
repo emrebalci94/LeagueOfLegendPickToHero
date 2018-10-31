0d41c32fd71172f3f0a7c77cd1ea97acbb647d16
Emre Balcı <palyosemre@gmail.com>
2018-10-31 21:46:49 +0300

    inventory_requirement added.

--- LeagueOfLegendPickToHero/Models/Support.cs
+++ LeagueOfLegendPickToHero/Models/Support.cs
@@ -9,13 +9,26 @@ namespace LeagueOfLegendPickToHero.Models
     class Support : Hero
     {
         private readonly HeroTypes _type;
-        public Support()
+        public Support(WeaponTypes weapon)
         {
 
             Name = HeroNameHelper.Instance.GetToRandomName();
             AttackPower = RandomHelper.Instance.Next(100);
             Hp = RandomHelper.Instance.Next(100);
             _type = HeroTypes.Support;
+            switch (weapon)
+            {
+                case WeaponTypes.Kılıç:
+                    AttackPower += 10;
+                    break;
+                case WeaponTypes.Silah:
+                    AttackPower += 20;
+                    break;
+                default:
+                    AttackPower += 0;
+                    break;
+            }
+
         }
 
         public override string GetToStatistics()
--- LeagueOfLegendPickToHero/Program.cs
+++ LeagueOfLegendPickToHero/Program.cs
@@ -8,6 +8,14 @@ namespace LeagueOfLegendPickToHero
 {
     class Program
     {
+        static void ForEachToEnum(Type _enum)
+        {
+            foreach (var item in Enum.GetValues(_enum))
+            {
+                Console.Write($"{item}({(int)item}) \t");
+
+            }
+        }
 
         static void Main(string[] args)
         {
@@ -22,11 +30,86 @@ namespace LeagueOfLegendPickToHero
             }
             Console.Write("\t :");
             bool isSelected = int.TryParse(Console.ReadLine(), out int selectedType);
-            
+
             if (isSelected && Enum.IsDefined(typeof(HeroTypes), selectedType))
             {
+
+                Console.Write("\t :");
                 Enum.TryParse(selectedType.ToString(), out HeroTypes heroType);
-                Hero hero = HeroCreaterHelper.Instance.GetHero(heroType);
+                Hero hero = null;
+                bool isHealthSelected, isWeaponSelected;
+                int selectedHealth, selectedWeapon;
+                switch (heroType)
+                {
+                    case HeroTypes.Warrior:
+                        Console.WriteLine("----------------LÜTFEN KAHRAMANINIZIN SİLAHINI SEÇİNİZ----------------");
+                        ForEachToEnum(typeof(WeaponTypes));
+                        Console.Write("\t :");
+                        isWeaponSelected = int.TryParse(Console.ReadLine(), out selectedWeapon);
+                        if (isWeaponSelected && Enum.IsDefined(typeof(WeaponTypes), selectedWeapon))
+                        {
+                            Enum.TryParse(selectedWeapon.ToString(), out WeaponTypes weapon);
+
+                            Console.WriteLine("----------------LÜTFEN KAHRAMANINIZIN SAĞLIĞINI SEÇİNİZ----------------");
+                            ForEachToEnum(typeof(HealthTypes));
+                            Console.Write("\t :");
+                            isHealthSelected = int.TryParse(Console.ReadLine(), out selectedHealth);
+                            if (isHealthSelected && Enum.IsDefined(typeof(HealthTypes), selectedHealth))
+                            {
+                                Enum.TryParse(selectedHealth.ToString(), out HealthTypes health);
+
+                                hero = new Warrior(weapon, health);
+                            }
+                            else
+                            {
+                                Console.WriteLine("--------------------------------------------------------------------");
+                                Console.WriteLine("Lütfen geçerli bir sağlık seçiniz.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("--------------------------------------------------------------------");
+                            Console.WriteLine("Lütfen geçerli bir silah seçiniz.");
+                        }
+                        break;
+                    case HeroTypes.Mage:
+                        Console.WriteLine("----------------LÜTFEN KAHRAMANINIZIN SAĞLIĞINI SEÇİNİZ----------------");
+                        ForEachToEnum(typeof(HealthTypes));
+                        Console.Write("\t :");
+                        isHealthSelected = int.TryParse(Console.ReadLine(), out selectedHealth);
+                        if (isHealthSelected && Enum.IsDefined(typeof(HealthTypes), selectedHealth))
+                        {
+                            Enum.TryParse(selectedHealth.ToString(), out HealthTypes health);
+                            hero = new Mage(health);
+                        }
+                        else
+                        {
+                            Console.WriteLine("--------------------------------------------------------------------");
+                            Console.WriteLine("Lütfen geçerli bir sağlık seçiniz.");
+                        }
+                        break;
+                    case HeroTypes.Support:
+                        Console.WriteLine("----------------LÜTFEN KAHRAMANINIZIN SİLAHINI SEÇİNİZ----------------");
+                        ForEachToEnum(typeof(WeaponTypes));
+                        Console.Write("\t :");
+                        isWeaponSelected = int.TryParse(Console.ReadLine(), out selectedWeapon);
+                        if (isWeaponSelected && Enum.IsDefined(typeof(WeaponTypes), selectedWeapon))
+                        {
+                            Enum.TryParse(selectedWeapon.ToString(), out WeaponTypes weapon);
+                            hero = new Support(weapon);
+                        }
+                        else
+                        {
+                            Console.WriteLine("--------------------------------------------------------------------");
+                            Console.WriteLine("Lütfen geçerli bir silah seçiniz.");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+
+
                 Console.WriteLine("--------------------------------------------------------------------");
                 Console.WriteLine(hero.GetToStatistics());
             }
@@ -39,11 +122,6 @@ namespace LeagueOfLegendPickToHero
             Console.WriteLine("--------------------------------------------------------------------");
             Console.WriteLine("-------------------KAPATMAK İÇİN BİR TUŞA BASINIZ-------------------");
 
-            //Hero Creater diye bir class yaratıp ordan getiricez.
-
-            //Hero hero = new Warrior();
-            //Console.WriteLine(hero.AttackPower);
-            //Console.WriteLine(hero.Hp);
             Console.ReadKey();
         }
     }
